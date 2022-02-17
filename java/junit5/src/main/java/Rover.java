520138c4ebf922f1d1991961a00a4e71b0939297
Guillaume <gcolsenet@coreoz.com>
2022-02-17 11:53:51 +0100

    [FEATURE] rover can move in straight lines  (with initial direction)

--- java/junit5/src/main/java/Rover.java
+++ java/junit5/src/main/java/Rover.java
@@ -1,17 +1,48 @@
 import org.javatuples.Pair;
 
-
 public class Rover {
 
-    private Pair<Integer, Integer> position = new Pair<>(0,0);
+    private Integer X = 0;
+    private Integer Y = 0;
+    private ORIENTATION orientation = ORIENTATION.SOUTH;
 
-    public Pair<Integer, Integer> getPosition() {
-        return this.position;
+    public Rover() {
+    }
+    // The initial seting is the landing one
+    public Rover (Pair<Integer, Integer> landingPoint, ORIENTATION landingOrientation) {
+        this.X = landingPoint.getValue0();
+        this.Y = landingPoint.getValue1();
+        this.orientation = landingOrientation;
     }
 
-    public void goBackward(int i) {
+
+    public Pair<Integer, Integer> getPosition() {
+        return Pair.with(this.X, this.Y);
+    }
+
+    public ORIENTATION getOrientation() {
+        return this.orientation;
     }
 
     public void goForward(int i) {
+        switch (this.orientation) {
+            case NORTH:
+                this.X -= i;
+                break;
+            case SOUTH:
+                this.X += i;
+                break;
+            case WEST:
+                this.Y -= i;
+                break;
+            case EST:
+                this.Y += i;
+                break;
+        }
     }
+
+    public void goBackward(int i) {
+        this.goForward(-i);
+    }
+
 }
--- java/junit5/src/test/java/RoverTest.java
+++ java/junit5/src/test/java/RoverTest.java
@@ -13,8 +13,19 @@ public class RoverTest {
     void rover_have_landed() {
         Rover firstRover = new Rover();
         Pair<Integer, Integer> position = firstRover.getPosition();
-        Pair<Integer, Integer> expectedPosition = new Pair<>(0,0);
+        Pair<Integer, Integer> expectedPosition = Pair.with(0,0);
         assertEquals(expectedPosition, position);
+    }
+
+    // The rover should be present at it's specified initial spot when created
+    @Test
+    void rover_have_landed_specify() {
+        Pair<Integer, Integer> expectedPosition = Pair.with(2,3);
+        Rover secondRover = new Rover(expectedPosition, ORIENTATION.NORTH);
+        Pair<Integer, Integer> position = secondRover.getPosition();
+        ORIENTATION orientation = secondRover.getOrientation();
+        assertEquals(position, expectedPosition);
+        assertEquals(orientation, ORIENTATION.NORTH);
     }
 
     // The rover should be able to go forward
@@ -38,4 +49,26 @@ public class RoverTest {
         Pair<Integer, Integer> expectedPosition = new Pair<>(-1,0);
         assertEquals(expectedPosition, position);
     }
+
+        // The rover should be able to go forward
+        @Test
+        void rover_advance_multiple() {
+            Rover firstRover = new Rover();
+            firstRover.goForward(5);
+            Pair<Integer, Integer> position = firstRover.getPosition();
+    
+            Pair<Integer, Integer> expectedPosition = new Pair<>(5,0);
+            assertEquals(expectedPosition, position);
+        }
+        
+        // The rover should be able to go backward
+        @Test
+        void rover_retreat_multiple() {
+            Rover firstRover = new Rover();
+            firstRover.goBackward(5);
+            Pair<Integer, Integer> position = firstRover.getPosition();
+    
+            Pair<Integer, Integer> expectedPosition = new Pair<>(-5,0);
+            assertEquals(expectedPosition, position);
+        }
 }
